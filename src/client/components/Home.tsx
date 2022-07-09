58cf95499e946f058cfd7bb83d4a41cfaa5e6bae
salle79 <salle79@gmail.com>
2022-07-09 08:41:00 +0200

    .

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -19,10 +19,10 @@ export const Home: React.FC = () => {
         <CardContent>
           <ImgStyled src='/assets/images/logo.png' />
           <Typography>
-            This is a starter kit to get you up and running with React & TypeScript on top of material-ui.
+            This is a starter kitttt to get you up and running with React & TypeScript on top of material-ui.
           </Typography>
           <Typography>
-            You can read more about how to share code between the client and the server at this{' '}
+            You can read more abouttttt how to share code between the client and the server at this{' '}
             <a href='https://medium.com/front-end-hacking/client-server-and-shared-code-846097c5260e'>
               medium blog post
             </a>
