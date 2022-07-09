d055471c25bf544c085a2416452480ae3299ff6e
salle79 <salle79@gmail.com>
2022-07-09 07:40:07 +0200

    BaseStack

---
+++ src/client/components/UsersList.tsx
@@ -0,0 +1,51 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import React from 'react';
+import { IUserDTO } from '../../shared/IUserDTO';
+import { getUserFullName } from '../../shared/utils';
+import { loadUsersAPI } from '../utils/api-facade';
+
+export const UsersList: React.FC = () => {
+  const [users, setUsers] = React.useState<IUserDTO[]>(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    let cancelled = false;
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      const users = await loadUsersAPI();
+      if (!cancelled) {
+        setUsers(users);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUsers();
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Users List' />
+          <CardContent>
+            <List>
+              {users.map((user) => (
+                <ListItem key={user.userId}>{getUserFullName(user)}</ListItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+      </Grid>
+  );
+};
